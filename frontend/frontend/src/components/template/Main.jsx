910ab775fa83a1e03511a786d431b662566bd983
Henrique Arantes <Henrique.ras3@gmail.com>
2024-09-16 22:33:06 -0300

    feat: add Main component

---
+++ frontend/frontend/src/components/template/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css';
+import Header from './Header';
+import React from 'react';
+
+
+
+export default props => 
+    <React.Fragment> 
+        <Header />
+        <main className="content">
+            Content
+        </main>      
+    </React.Fragment>
+
