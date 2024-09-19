86c20f1f92851866c51a90d18c29340274b07075
Henrique Arantes <Henrique.ras3@gmail.com>
2024-09-19 12:34:09 -0300

    feat: changing the main to home

---
+++ frontend/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import Main from '../template/Main';
+
+export default props =>
+    <Main icon="home" tittle="start."
+    subtittle="Second project from react.">
+    <div className="display-4">Welcome!</div>
+    <hr />
+    <p className="mb-0">A system to demonstrate a construction of a CRUD in React!</p>
+
+    </Main>
