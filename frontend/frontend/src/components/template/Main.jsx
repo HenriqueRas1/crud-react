aba2aaf255123231bb6c5c0a36d48929fd28960d
Henrique Arantes <Henrique.ras3@gmail.com>
2024-09-19 12:33:47 -0300

    feat: main creation

--- frontend/frontend/src/components/template/Main.jsx
+++ frontend/frontend/src/components/template/Main.jsx
@@ -7,8 +7,10 @@ import React from 'react';
 export default props => 
     <React.Fragment> 
         <Header {...props}/>
-        <main className="content">
-            Content
+        <main className="content container-fluid">
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
         </main>      
     </React.Fragment>
 
