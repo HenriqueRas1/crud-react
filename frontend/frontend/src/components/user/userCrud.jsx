8aeed8cdca9ca4a5865561e68c96d992c4e72d76
Henrique Arantes <Henrique.ras3@gmail.com>
2024-10-17 18:21:10 -0300

    feat: HeaderProps creation

--- frontend/frontend/src/components/user/userCrud.jsx
+++ frontend/frontend/src/components/user/userCrud.jsx
@@ -1,2 +1,8 @@
 import React, { Component } from 'react'
- 
+import Main from '../template/Main'
+
+const headerProps = {
+    icon: 'users',
+    title: 'usuarios',
+    subtitle: 'Cadastro de usuarios: incluir, Listar, Alterar e Excluir'
+}
