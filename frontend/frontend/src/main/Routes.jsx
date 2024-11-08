8881d705906449e78c8e43ed6d54a0a235563c6e
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-08 15:51:58 -0300

    feat: Routes component creation

---
+++ frontend/frontend/src/main/Routes.jsx
@@ -0,0 +1,25 @@
+import { Switch, Route, Redirect } from 'react-router'
+
+import Home from '../components/home/Home'
+import UserCrud from '../components/user/userCrud'
+
+export default props => 
+    <Switch> 
+        /* configurando uma rota para a pagina principal da aplicação*/ 
+        <Route exact path='/' component={Home} />
+    </Switch>
+
+
+
+
+
+/* 
+<Route>: É um componente do React Router que representa uma rota, ou seja, uma URL específica na sua aplicação web.
+
+exact: Essa prop indica que a rota deve corresponder exatamente à URL, sem nenhum caractere a mais ou a menos.
+
+path='/': Define o caminho da URL que essa rota irá atender, nesse caso, a raiz da aplicação (/).
+
+component={Home}: Especifica qual componente React será renderizado quando essa rota for acessada. 
+Nesse caso, o componente Home será exibido quando o usuário acessar a URL raiz.
+*/ 
