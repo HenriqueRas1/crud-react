7f07310d3ac0005ea236ed195ebbd733544d9ca0
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-08 16:01:38 -0300

    feat: import BrowserRouter

--- frontend/frontend/src/main/App.jsx
+++ frontend/frontend/src/main/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
 
 import Logo from '../components/template/Logo'
