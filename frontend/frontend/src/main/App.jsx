7a30801392abeaf13a33cf062a3eae3a9e8a52ae
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-11 10:53:02 -0300

    need to fix

--- frontend/frontend/src/main/App.jsx
+++ frontend/frontend/src/main/App.jsx
@@ -4,9 +4,11 @@ import 'font-awesome/css/font-awesome.min.css'
 import { BrowserRouter } from 'react-router-dom'
 
 
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+//import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 
