819e22cf2ea7628282bd6a319256c16e509bef06
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-13 10:33:09 -0300

    need to fix later

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-
-
-
 import Home from '../components/home/Home'
 import UserCrud from "../components/user/UserCrud.jsx";
 
 export default props => (
     <Routes> 
         <Route exact path="/" element={<Home />} />
-        <Route path="/users" element={<UserCrud />} />
+        <Route path="/user" element={<UserCrud />} />
         <Route path="*" element={<Home />} />
     </Routes>
 );
