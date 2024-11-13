97412d55f0a47b526f1d72449f4d634895df8cd6
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-13 10:03:22 -0300

    feat: add link and remove the hash

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,13 +1,16 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom' 
 
-export default props => 
+export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <a href="#">
+            {/* Refatorar em casa! */}
+            <Link to="/">
                 <i className="fa fa-home"></i> Start
-            </a>
-            <a href="#/users">
-                <i className="fa fa-users"></i> Users</a>
+            </Link>
+            <Link to="/users">
+                <i className="fa fa-users"></i> Users
+            </Link>
         </nav>
     </aside>
