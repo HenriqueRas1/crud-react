52c61d06d0c5715ee1f4d52cd7dcf56becd89204
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-13 10:48:00 -0300

    fix

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -4,13 +4,15 @@ import React from 'react';
 
 
 
-export default props => 
-    <React.Fragment> 
-        <Header {...props}/>
+export default props =>
+    <React.Fragment>
+        <Header {...props} />
         <main className="content container-fluid">
             <div className="p-3 mt-3">
                 {props.children}
             </div>
-        </main>      
+        </main>
     </React.Fragment>
+
+
 
