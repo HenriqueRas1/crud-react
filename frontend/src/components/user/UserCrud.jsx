8f3f9ea08dd6831afc5b863a4b94af8295245b50
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-14 11:33:36 -0300

    feat: gerenciamento de usuario

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -8,8 +8,26 @@ const headerProps = {
     title: 'Users',
     subtitle: 'Cadastro de usuarios: incluir, Listar, Alterar e Excluir'
 }
+// gerenciamento de usuarios 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
+
+
+
+
+
+
 // componente de classe para metodo de ciclo de vida react e estado
 export default class UserCrud extends Component {
+    
+    state = {...initialState}
+
+
+    
     render() {
         return (
             <Main {...headerProps}>
