38f8799afc340e1412139ab7c88467badfd76824
Henrique Arantes <Henrique.ras3@gmail.com>
2024-11-14 10:33:26 -0300

    fix: home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import Main from '../template/Main';
 
-export default props =>
-    <Main icon="home" tittle="start."
-    subtittle="Second project from react.">
-    <div className="display-4">Welcome!</div>
-    <hr />
-    <p className="mb-0">A system to demonstrate a construction of a CRUD in React!</p>
 
+export default props =>
+    <Main icon="home" title="Start"
+        subtitle="Second React Project">
+        <div className='display-4'>Welcome!</div>
+        <hr />
+        <p className="mb-0">A system to demonstrate a construction of a CRUD in React!</p>
     </Main>
