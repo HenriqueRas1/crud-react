2622fbf97e3d6bfd869153b4b0f7114d35fa6e3b
Henrique Arantes <Henrique.ras3@gmail.com>
2025-01-07 19:58:56 -0300

    feat: button

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -80,12 +80,12 @@ export default class UserCrud extends Component {
                 <hr /> 
                 <div className="row"> 
                     <div className="col-12 d-flex justify-content-end">
-                        <button className="btn btn-primary">
+                        <button className="btn btn-primary"> 
                             Salvar
                         </button>
 
                         <button className="btn btn-secondary ml-2">
-                            
+                            Cancelar
                         </button>
                     </div>
                 </div>
